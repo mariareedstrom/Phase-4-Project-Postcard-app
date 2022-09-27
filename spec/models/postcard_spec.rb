c710f18b749ed43535b9c64e7381b3fee40fdaef
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-09-27 14:18:30 -0500

    update postcard tests

--- spec/models/postcard_spec.rb
+++ spec/models/postcard_spec.rb
@@ -4,20 +4,23 @@ RSpec.describe Postcard, type: :model do
   it "can be created successfully with valid data" do
 
     user = User.create(name: "Bob Bobson", username: "bob@email.com", password: "sosecure")
+    destination = Destination.create(name: "Austin")
     postcard = Postcard.create(greeting: "Hi there, We are on vacation in Rome! We have seen the Colosseum and the Trevi Fountain today. Next we are going to the Spanish Steps. The food and weather is great. Wish you were here! xox ",
                                image_url: "https://i.pinimg.com/originals/92/dd/d9/92ddd99ae5276480d3089e72decb2a85.jpg",
-                               user_id: user.id
+                               user_id: user.id,
+                               destination_id: destination.id
                                )
     expect(postcard).to be_valid
   end
 
   describe "Associations" do
     it { should belong_to(:user) }
+    it { should belong_to(:destination) }
   end
 
   describe "Validations" do
     it { is_expected.to validate_presence_of(:greeting) }
-    it { is_expected.to validate_length_of(:greeting).is_at_least(150).is_at_most(250) }
+    it { is_expected.to validate_length_of(:greeting).is_at_least(50).is_at_most(250) }
     it { is_expected.to validate_presence_of(:image_url) }
   end
 
